58c60fa459660530494edd99a1a823133bfdf6bd
Josias2910 <JosiasSchumacher@gmail.com>
2024-10-14 18:59:04 -0300

    asd

--- Guia6.1 - Ejercicio 1/Guia6.1 - Ej1 - Patentes/RegexProcesableImpl.cs
+++ Guia6.1 - Ejercicio 1/Guia6.1 - Ej1 - Patentes/RegexProcesableImpl.cs
@@ -12,18 +12,30 @@ namespace Guia6._1___Ej1___Patentes
         public string Procesar(string patente, out string formateada)
         {
             formateada = "";
-            Match m = null;
-            m = Regex.Match(patente, @"[A-Z]{3}\s?\d{3}", RegexOptions.IgnoreCase);
+            Match m = null;           
+            m = Regex.Match(patente, @"[A-Z]{3}\s?\d{3}$", RegexOptions.IgnoreCase);
             if (m.Success)
             {
                 formateada = m.Value.Replace(" ", "");
                 return $"Automoviles y Camionetas hasta 2016 - Regex";
-            }
-            m = Regex.Match(patente, @"[A-Z]{2}\s?\d{3}\s?[A-Z]{2}", RegexOptions.IgnoreCase);
+            }          
+            m = Regex.Match(patente, @"[A-Z]{2}\s?\d{3}\s?[A-Z]{2}$", RegexOptions.IgnoreCase);
             if (m.Success)
             {
                 formateada = m.Value.Replace(" ","");
                 return $"Automoviles y Camionetas desde 2016 - Regex";
+            }            
+            m = Regex.Match(patente, @"[A-Z]{2}\s?\d{3}\s?[A-Z]{3}$", RegexOptions.IgnoreCase);
+            if (m.Success)
+            {
+                formateada = m.Value.Replace(" ", "");
+                return $"Motocicletas - Regex";
+            }            
+            m = Regex.Match(patente, @"[A-Z]{2}\s?\d{4}$", RegexOptions.IgnoreCase);
+            if (m.Success)
+            {
+                formateada = m.Value.Replace(" ", "");
+                return $"Acoplado - Regex";
             }
             return formateada;
         }
--- Guia6.1 - Ejercicio 1/Guia6.1 - Ej1 - Patentes/StringProcesableImpl.cs
+++ Guia6.1 - Ejercicio 1/Guia6.1 - Ej1 - Patentes/StringProcesableImpl.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -76,7 +77,78 @@ namespace Guia6._1___Ej1___Patentes
                 {
                     formateada = patenteNormalizada;
                     return "Automoviles y Camionetas desde 2016 - String";
+                }              
+            }
+
+            if (patenteNormalizada.Length == 8)
+            {
+                bool motocicleta = true;
+                for (int i = 0; i < patenteNormalizada.Length; ++i)
+                {
+                    if (i < 2)
+                    {
+                        if (!char.IsLetter(patenteNormalizada[i]))
+                        {
+                            motocicleta = false;
+                            break;
+                        }
+                    }
+                    else if (i < 5)
+                    {
+                        if (!char.IsNumber(patenteNormalizada[i]))
+                        {
+                            motocicleta = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (!char.IsLetter(patenteNormalizada[i]))
+                        {
+                            motocicleta = false;
+                            break;
+                        }
+                    }
                 }
+                if (motocicleta)
+                {
+                    formateada = patenteNormalizada;
+                    return $"Motocicletas = String";
+                }
+            }
+
+            if (patenteNormalizada.Length == 6)
+            {
+                bool acoplado = true;
+                for (int i = 0; i < patenteNormalizada.Length; i++)
+                {
+                    if (i < 2)
+                    {
+                        if (!char.IsLetter(patenteNormalizada[i]))
+                        {
+                            acoplado = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if (!char.IsNumber(patenteNormalizada[i]))
+                        {
+                            acoplado = false;
+                            break;
+                        }
+                    }
+                }
+                if (acoplado)
+                {
+                    formateada = patenteNormalizada;
+                    return $"Acoplado - String";
+                }
+            }
+
+            if (patenteNormalizada.Length != 6 && patenteNormalizada.Length != 8 && patenteNormalizada.Length != 7)
+            {
+                return $"No Reconocido";
             }
             return formateada;
         }
